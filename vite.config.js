20e24172d993afe068d2684c3dd1e10ba7d4420e
Alessandro Santoemma <alessandro.santoemma@gmail.com>
2026-04-21 14:03:37 +0200

    tailwind

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,11 @@
-import { sveltekit } from '@sveltejs/kit/vite';
+import { sveltekit } from '@sveltejs/kit/vite'; // Se usi SvelteKit
+// import { svelte } from '@sveltejs/vite-plugin-svelte'; // Se usi Svelte puro (senza Kit)
+import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+    plugins: [
+        tailwindcss(),
+        sveltekit() // o svelte() se non usi Kit
+    ]
 });
